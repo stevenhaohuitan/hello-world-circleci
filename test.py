274fb9bee220dbea247b21c07d01a0a7c1ff1ee2
Steven Tan <stan@solution-tek.com>
2018-06-12 15:50:06 -0400

    test1

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
-    name = raw_input("What is your name? \n")
-    age = raw_input("What is your age? \n")
-    country = raw_input("What country are you from? \n")
+    name = raw_input("What is your name? ")
+    age = raw_input("What is your age?")
+    country = raw_input("What country are you from?")
     print "Hello, %s." % name
     print "Your age is %s." % age
     print "You are from %s." % country
